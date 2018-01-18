2aec11fbc7becbafbcd92fcebfbe33793c7b3f89
HTC Training <31919061+HTCTraining@users.noreply.github.com>
2018-01-18 10:34:25 +0530

    Day-3 classworks

---
+++ Servlet/Day-3/testapp/src/com/htc/servlet/service/UserService.java
@@ -0,0 +1,36 @@
+package com.htc.servlet.service;
+
+import java.sql.SQLException;
+
+import com.htc.servlet.dao.UserDAO;
+import com.htc.servlet.dao.UserDAOImpl;
+import com.htc.servlet.dto.UserDTO;
+
+public class UserService {
+
+	public boolean registerUser(String username, String password, String email, String mobileno) {
+		
+		UserDAO userdao = new UserDAOImpl();
+		boolean status = false;
+		try {
+			status = userdao.registerUser(username, password, email, mobileno);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+	
+	
+	public UserDTO searchUser(String username) {
+		UserDAO userdao = new UserDAOImpl();
+		boolean status = false;
+		UserDTO userDTO = null;
+		try {
+			userDTO = userdao.searchUser(username);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userDTO;
+	
+	}
+}
